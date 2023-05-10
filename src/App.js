7498b756fe0f744fc32a1946703ed9ae55cc9157
kyial05 <asuskyial@gmail.com>
2023-05-10 18:13:48 +0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React from "react";
 import "./App.css";
 
 export const App = () => {
-  return <div className="App">
-    sdfghjkl;''
-  </div>;
+  return (
+    <div className="App">
+      new
+      <h1>hello world</h1>
+    </div>
+  );
 };
